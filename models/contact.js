0b52a38deae56d441c81642f3826fd9c2a8b9603
vibek44 <kshetrivibek@gmail.com>
2026-04-18 16:44:01 +0300

    moved mongoose connection code to app.js and only responsible for schema and model creation

--- models/contact.js
+++ models/contact.js
@@ -1,45 +1,30 @@
-require('dotenv').config()
 const mongoose=require('mongoose')
 
-const url = process.env.MONGODB_URI
-mongoose.set('strictQuery', false)
-mongoose.connect(url, { family:4 })
-   .then(()=>{
-       console.log('connected')
-   })
-   .catch(error=>{
-        console.log(`error conecting...`) 
-   })
-
 const contactSchema=new mongoose.Schema({
-    name:{ 
-       type:String,
-       required:true,
-       unique:true,
-       minLength:[3,' length should be atleast three charactor long']
-    },
-    number:{
-        type:String,
-        required:true,
-        minLength:[8, ' should be atleast eight digit long'],
-        validate:{
-            validator:function(v){
-                return /\d{2,3}-\d{7,8}/.test(v) && v.split('-')[0].length<4 && v.split('-')[1].length<9
-            },
-            message:props=>`${props.value} is not a valid phone number`          
-        }
+  name: {
+    type:String,
+    required:true,
+    unique:true,
+    minLength:[3,' length should be atleast three charactor long']
+  },
+  number: {
+    type:String,
+    required:true,
+    minLength:[8, ' should be atleast eight digit long'],
+    validate:{
+      validator:function(v){
+        return /\d{2,3}-\d{7,8}/.test(v) && v.split('-')[0].length<4 && v.split('-')[1].length<9
+      },
+      message:props => `${props.value} is not a valid phone number`
     }
-
+  }
 })
 
 contactSchema.set('toJSON', {
-    transform: (document, returnedObject) =>{
-        returnedObject.id=returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id=returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
-  
-   
-
 module.exports=mongoose.model('Contact', contactSchema)
