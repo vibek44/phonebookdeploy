0f337a2f4539526bce24b5a8bdb7f1811fd0ebde
vibek44 <kshetrivibek@gmail.com>
2026-04-18 16:42:11 +0300

    moving express app creation from index file  to its own module and code refactoring

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-const express=require('express')
-const app=express()
-const {infoRouter,personsRouter}=require('./controller/UserPage')
-app.use(express.static('dist'))
-app.use(express.json())
-const { unKnownEndPoint,errorHandler }=require('./utilities/middleware')
-const logger=require('./utilities/logger')
+const config=require('./utilities/config')
+const app=require('./app')
 
-    
-const PORT= process.env.PORT || 3001
+const PORT=config.PORT
 
-app.use(logger)
-
-app.use('/api',[infoRouter,personsRouter])
-app.use(unKnownEndPoint)
-app.use(errorHandler)
-
-app.listen(PORT,()=>{
-    console.log(`server running on port : ${PORT}`);
+app.listen(PORT,() => {
+  console.log(`server running on port : ${PORT}`)
 })
 
 
